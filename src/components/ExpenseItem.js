dbe620e1bc1e465c99324ace20f3945b4fa9b617
Reza Sabourinejad <reza.sabourinejad@gmail.com>
2023-10-07 10:22:29 +0330

    first phase of project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+import ExpensDate from "./ExpensDate";
+import "./ExpensItem.css";
+
+function ExpensItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpensDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpensItem;
